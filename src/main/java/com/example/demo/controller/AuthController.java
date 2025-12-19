63b99afafa75d60599497cb9d9e27a2e314f4df4
bala-amypo <bala@amypo.in>
2025-12-19 10:12:17 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,13 +1,9 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.AuthRequest;
 import com.example.demo.dto.LoginRequest;
-import com.example.demo.repository.UserRepository;
-import com.example.demo.security.JwtTokenProvider;
+import com.example.demo.dto.RegisterRequest;
 import com.example.demo.service.UserService;
-import com.example.demo.service.impl.UserServiceImpl;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,24 +12,17 @@ public class AuthController {
 
     private final UserService service;
 
-    // TEST CONSTRUCTOR
-    public AuthController(UserRepository repo,
-                          PasswordEncoder encoder,
-                          JwtTokenProvider jwt) {
-        this.service = new UserServiceImpl(repo, encoder, jwt);
-    }
-
-    // SPRING CONSTRUCTOR
     public AuthController(UserService service) {
         this.service = service;
     }
 
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterRequest req) {
+        return ResponseEntity.ok(service.registerUser(req));
+    }
+
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody AuthRequest request) {
-        return ResponseEntity.ok(
-                service.loginUser(
-                        new LoginRequest(request.getEmail(), request.getPassword())
-                )
-        );
+    public ResponseEntity<?> login(@RequestBody LoginRequest req) {
+        return ResponseEntity.ok(service.loginUser(req));
     }
 }
