08c9c7d7c68cf263883991ceec4aaf79df1aedab
bala-amypo <bala@amypo.in>
2025-12-21 16:31:25 +0000

    User commit

--- src/main/java/com/example/demo/WarrantyClaimApplication.java
+++ src/main/java/com/example/demo/WarrantyClaimApplication.java
@@ -1,11 +1,11 @@
-package com.example.demo;
+// package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class WarrantyClaimApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(WarrantyClaimApplication.class, args);
-    }
-}
+// @SpringBootApplication
+// public class WarrantyClaimApplication {
+//     public static void main(String[] args) {
+//         SpringApplication.run(WarrantyClaimApplication.class, args);
+//     }
+// }
