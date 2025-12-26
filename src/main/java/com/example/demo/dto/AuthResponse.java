3edf70739133013d620223103eb63f26a4f51028
bala-amypo <bala@amypo.in>
2025-12-26 18:26:07 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -1,27 +1,35 @@
 package com.example.demo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Set;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponse {
     private String token;
     private String email;
     private String name;
     private Set<String> roles;
     
-    // No-args constructor
+    // If you're not using Lombok, create these constructors manually:
     public AuthResponse() {}
     
-    // All-args constructor
+    public AuthResponse(String token, String email) {
+        this.token = token;
+        this.email = email;
+    }
+    
     public AuthResponse(String token, String email, String name, Set<String> roles) {
         this.token = token;
         this.email = email;
         this.name = name;
         this.roles = roles;
-    }
-    
-    // Builder pattern manually
-    public static AuthResponseBuilder builder() {
-        return new AuthResponseBuilder();
     }
     
     // Getters and setters
@@ -36,36 +44,4 @@ public class AuthResponse {
     
     public Set<String> getRoles() { return roles; }
     public void setRoles(Set<String> roles) { this.roles = roles; }
-    
-    // Builder class
-    public static class AuthResponseBuilder {
-        private String token;
-        private String email;
-        private String name;
-        private Set<String> roles;
-        
-        public AuthResponseBuilder token(String token) {
-            this.token = token;
-            return this;
-        }
-        
-        public AuthResponseBuilder email(String email) {
-            this.email = email;
-            return this;
-        }
-        
-        public AuthResponseBuilder name(String name) {
-            this.name = name;
-            return this;
-        }
-        
-        public AuthResponseBuilder roles(Set<String> roles) {
-            this.roles = roles;
-            return this;
-        }
-        
-        public AuthResponse build() {
-            return new AuthResponse(token, email, name, roles);
-        }
-    }
 }
